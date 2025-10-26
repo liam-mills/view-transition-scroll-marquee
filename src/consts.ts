7250cafad2eee2a292c23f7397c81f14f7e12e69
liam-mills <liammills.web@gmail.com>
2025-10-26 12:52:02 +0000

    chore: update site title const

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Liam Mills';
+export const SITE_TITLE = 'View Transition Scroll Marquee';
 export const SITE_DOMAIN = 'https://liam-mills.github.io';
 export const REPO_NAME = 'view-transition-scroll-marquee'
